7671ce042ed36cbd67e7fb5b76aab7c1f93ed8ad
Administrador <Administrador@G-Desktop.router-lks>
2014-04-06 17:28:39 -0300

    1° commit

--- src/proyectouni/Conexion.java
+++ src/proyectouni/Conexion.java
@@ -6,10 +6,37 @@
 
 package proyectouni;
 
+import java.sql.*;
+
 /**
  *
  * @author Administrador
  */
 public class Conexion {
-    
+    /* datos para la conexion */
+    private String bd = "db_programacion";
+    private String login = "dbprogramacion";
+    private String password = "dbprogramacion";
+    private String url = "jdbc:mysql//localhost/"+bd;
+    private Connection conn = null;
 }
+public Conexion(){
+      try{
+         //obtenemos el driver de para mysql
+         Class.forName("com.mysql.jdbc.Driver");
+         //obtenemos la conexión
+         conn = DriverManager.getConnection(url,login,password);
+         if (conn!=null){
+            System.out.println("OK base de datos "+bd+" listo");
+         }
+      }catch(SQLException e){
+         System.out.println(e);
+      }catch(ClassNotFoundException e){
+         System.out.println(e);
+      }
+    }
+
+
+
+
+
